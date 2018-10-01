531ebffdf67ff3c62e267462b1a38cfc6893b837
Eva <evamatematica92@gmail.com>
2018-10-01 11:16:02 +0200

    Fundamentos

---
+++ Fundamentos01/src/es/cj/fundamentos/datos/Principal.java
@@ -0,0 +1,34 @@
+package es.cj.fundamentos.datos;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TIPOS VARIABLES
+		
+		// TIPOS ENTEROS
+		byte b; // -128 a +127 (8 bits)
+		short sh; // -32768 a +32767 (16 bits)
+		int i; // (32 bits)
+		long l; // (64 bits)
+		
+		char c; // Sirve para almacenar un caracter -> de 0 a 65535
+		char ch = 71;
+		System.out.println(ch);
+		
+		// TIPOS REALES
+		float f = 3.1416F; // (32 bits)		
+		double d = 3.1416; // (64 bits)
+		
+		double pi = Math.PI;
+		System.out.println(pi);
+		
+		// TIPO LÓGICO
+		boolean bol = true /*(o false)*/;
+		
+		// TIPOS COMPLEJOS
+		Byte by;
+		Integer ii;
+		Short sho;
+		
+	}
+}
