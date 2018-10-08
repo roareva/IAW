9ac924204f7711e9493d175cddd2c40db5e455ca
Eva <evamatematica92@gmail.com>
2018-10-08 10:33:31 +0200

    Commit ejercicios IF

---
+++ EjercicioIF/src/es/cj/ejerciciosif/datos/Ejercicio02.java
@@ -0,0 +1,45 @@
+package es.cj.ejerciciosif.datos;
+
+import java.util.Scanner; 
+
+public class Ejercicio02 {
+	
+	//Pedir al usuario día, mes y año y comprobar que la fecha es correcta incluidos bisiestos
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		System.out.println("Día: ");
+		int dia = sc.nextInt();
+		
+		System.out.println("Mes: ");
+		int mes = sc.nextInt();
+		
+		System.out.println("Año: ");
+		int anyo = sc.nextInt();
+		
+		boolean fechaCorrecta = false;
+		
+		if (anyo >= 0) {
+			boolean esBisiesto = ((anyo % 4 == 0 && anyo % 100 != 0) || (anyo % 400 == 0));
+			if (mes >= 1 && mes <= 12) {
+				switch (mes) {
+				case 2:
+					if (dia >= 1 && dia <= 29 && esBisiesto)
+						fechaCorrecta = true;
+					else if (dia >= 1 && dia <= 28 && !esBisiesto)
+						fechaCorrecta = true;
+					break;
+
+				default:
+					break;
+				}
+			}
+		}
+		if (fechaCorrecta)
+			System.out.println("Fecha correcta: " + dia + "/" + mes + "/" + anyo);
+		else
+			System.out.println("Fecha incorrecta: " + dia + "/" + mes + "/" + anyo);
+		
+		sc.close();
+	}
+
+}
