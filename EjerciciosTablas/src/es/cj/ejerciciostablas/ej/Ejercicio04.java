77923c0e081cf01bac5393fbfbf1334e49ad9d96
Eva <evamatematica92@gmail.com>
2018-10-26 13:03:18 +0200

    Commit

--- EjerciciosTablas/src/es/cj/ejerciciostablas/ej/Ejercicio04.java
+++ EjerciciosTablas/src/es/cj/ejerciciostablas/ej/Ejercicio04.java
@@ -1,9 +1,25 @@
 package es.cj.ejerciciostablas.ej;
 
-public class Ejercicio04 {
+import java.util.Scanner;
 
+public class Ejercicio04 {
+	//Leer 2 tablas de n números y mezclarla en una tercera tabla de la siguiente forma: 1º de A, 1º de B, 
+	//2º de A, 2º de B, 3º de A, 3º de B,...
+	private static Scanner sc = new Scanner(System.in);
+	
+	private static int [] tabla1;
+	
+	private static int [] tabla2;
+	
+	private static int [] tabla3;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int tamanyo;
+		do {
+			System.out.println("Nº de elementos de ambas tablas: ");
+			tamanyo = sc.nextInt();
+		} while (tamanyo <= 0);
+		//numeros = new int [tamanyo];
 
 	}
 
